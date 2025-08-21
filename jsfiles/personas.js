dbdb6153b9e9e0806c64b611cfb7fea275712c49
GoguX76 <se.godoyr@duocuc.cl>
2025-08-21 15:48:22 -0400

    Archivos creandose para actividad de personas

---
+++ jsfiles/personas.js
@@ -0,0 +1,7 @@
+var nom = prompt("Ingrese su nombre")
+var aps = prompt("Ingrese sus apellidos")
+
+function identidadPersona(nombre, apellido) {
+
+    return nombre.toUpperCase() + " " + apellido.toUpperCase()
+}
